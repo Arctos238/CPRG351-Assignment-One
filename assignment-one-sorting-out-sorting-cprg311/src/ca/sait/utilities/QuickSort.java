dc1af1522fec2cf7955d2e7fb8252c14ec9ca84b
Jacque Pointer <93753375+Arctos238@users.noreply.github.com>
2022-09-28 21:53:08 -0600

    All Algorithms Sorted

--- assignment-one-sorting-out-sorting-cprg311/src/ca/sait/utilities/QuickSort.java
+++ assignment-one-sorting-out-sorting-cprg311/src/ca/sait/utilities/QuickSort.java
@@ -6,11 +6,73 @@ import ca.sait.shapeDomain.Shape;
 
 public class QuickSort {
 	public QuickSort(Shape[] shapeArray, Comparator<Shape> comparator) {
-
+		quickSort(shapeArray, 0, shapeArray.length - 1, comparator);
 	}
 
 	public QuickSort(Shape[] shapeArray) {
-  
+		quickSort(shapeArray, 0, shapeArray.length - 1);
+	}
+	
+	private void quickSort(Shape[] arr, int low, int high, Comparator<Shape> comparator) {
+		if (low < high) {
+
+			int pi = partition(arr, low, high, comparator);
+
+			quickSort(arr, low, pi - 1, comparator);
+			quickSort(arr, pi + 1, high, comparator);
+		}
+	}
+	
+	private int partition(Shape[] arr, int low, int high, Comparator<Shape> comparator) {
+
+		Shape pivot = arr[high];
+
+		int i = (low - 1);
+
+		for (int j = low; j <= high - 1; j++) {
+
+			if (comparator.compare(arr[j], pivot) == 100) {
+
+				i++;
+				swap(arr, i, j);
+			}
+		}
+		swap(arr, i + 1, high);
+		return (i + 1);
+	}
+	
+	private void quickSort(Shape[] arr, int low, int high) {
+		if (low < high) {
+
+			int pi = partition(arr, low, high);
+
+			quickSort(arr, low, pi - 1);
+			quickSort(arr, pi + 1, high);
+		}
+	}
+	
+	private int partition(Shape[] arr, int low, int high) {
+
+		Shape pivot = arr[high];
+
+		int i = (low - 1);
+
+		for (int j = low; j <= high - 1; j++) {
+
+			if (arr[j].compareTo(pivot) == 1) {
+
+				i++;
+				swap(arr, i, j);
+			}
+		}
+		swap(arr, i + 1, high);
+		return (i + 1);
+	}
+
+	private void swap(Shape[] arr, int i, int j) {
+		Shape temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 
 }
